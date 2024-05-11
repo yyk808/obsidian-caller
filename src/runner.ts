ba0fecd7fd36be7e099d0e9edaaec7fe42c102ab
Neon <yyk1249501542@gmail.com>
2024-05-11 13:29:27 +0800

    feat: expand available stdin variables.

--- src/runner.ts
+++ src/runner.ts
@@ -2,6 +2,10 @@ import { exec } from "child_process";
 import { App, Notice } from "obsidian";
 import { Command } from "@/command";
 
+const UNDEFINED = "undefined";
+const TRUE = "true";
+const FALSE = "false";
+
 interface ArgMap {
     [key: string]: string,
 }
@@ -15,14 +19,32 @@ function parseStringTemplate(str: string, obj: ArgMap) {
 }
 
 function getEnvVars(app: App) {
-    const APPID = app.appId;
+    const vault = app.vault;
+    const editor = app.workspace.activeEditor;
+    const cursor = editor?.editor?.getCursor();
+
+    const cur_file = editor?.file?.path;
+    const cur_line_number = cursor?.line;
+    const cur_line = cur_line_number ? editor?.editor?.getLine(cur_line_number) : undefined;
+    const selected = editor?.editor?.getSelection();
 
     return {
-        "APPID": APPID,
+        "APPID": app.appId,
+        "TITLE": app.getAppTitle(),
+        "VAULT_ROOT": vault.getRoot().path,
+        "VAULT_NAME": vault.getName(),
+        "CONFIG_PATH": vault.configDir,
+
+        "CUR_FILE": cur_file ? cur_file : UNDEFINED,
+        "CUR_LINE_NUM": cur_line_number ? cur_line_number.toString() : UNDEFINED,
+        "CUR_LINE": cur_line ? cur_line : UNDEFINED,
+        "SELECTED": selected ? selected : UNDEFINED,
+        "IS_MOBILE": app.isMobile ? TRUE : FALSE,
+
     };
 }
 
-export async function runCommand(cmd: Command, app: App) {
+export async function runCommand(cmd: Command, app: App, options?: undefined) {
     let cwd = cmd.cwd != "" ? cmd.cwd : undefined;
     let args: string;
     if (cmd.arguments != "") {
